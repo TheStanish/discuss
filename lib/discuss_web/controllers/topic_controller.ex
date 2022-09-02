480d600de326c339242c6aaddfb635186140eb23
TheStanish <tstan2310@gmail.com>
2022-09-02 11:39:51 -0400

    added edit and update functions to TopicController, added edit buttons to topic list

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -9,7 +9,7 @@ defmodule DiscussWeb.TopicController do
     render conn, "index.html", topics: topics
   end
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
@@ -19,15 +19,36 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
    case Repo.insert(changeset) do
-     {:ok, post} ->
+     {:ok, _topic} ->
       conn
       |> put_flash(:info, "Topic created!")
       |> redirect(to: Routes.topic_path(conn, :index))
      {:error, changeset} ->
       render conn, "new.html", changeset: changeset
       # conn
-      # |> put_flash(:warning, "Topic cannot be black!")
+      # |> put_flash(:warning, "Topic cannot be blank!")
       # |> redirect(to: Routes.topic_path(conn, :new))
    end
+  end
+
+  def edit(conn, %{"id" => topic_id }) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic updated successfully!")
+        |> redirect(to: Routes.topic_path(conn, :index))
+        {:error, changeset} ->
+          render conn, "edit.html", changeset: changeset, topic: old_topic
+    end
   end
 end
